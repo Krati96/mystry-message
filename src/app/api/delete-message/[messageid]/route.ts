0a84556d574a504235040ce1b5016929d170d618
Rishabh <shahbab.alam@bigohtech.com>
2024-07-31 18:00:58 +0530

    message-delete route

---
+++ src/app/api/delete-message/[messageid]/route.ts
@@ -0,0 +1,41 @@
+import { getServerSession } from "next-auth";
+import { authOptions } from "../../auth/[...nextauth]/options";
+import dbConnect from "@/lib/dbConnect";
+import UserModel from "@/model/User";
+import { User } from "next-auth";
+
+export async function DELETE(request:Request, {params}: {params: {messageid:string}}) {
+    const messageId = params.messageid
+    await dbConnect()
+    const session = await getServerSession(authOptions) 
+    const user: User = session?.user as User
+    if(!session || !session?.user){
+        return Response.json({
+            success: false,
+            message: 'Not Authenticated'
+        },{status: 401})
+    }
+    
+    try {
+        const updateResult = await UserModel.updateOne(
+            {id: user._id},
+            {$pull: {messages: {_id:messageId}}}
+        )
+        if(updateResult.modifiedCount == 0){
+            return Response.json({
+                success: false,
+                message: 'Message not found or already delete'
+            },{status: 404})
+        }
+        return Response.json({
+            success: true,
+            message: 'Message Deleted Successfully'
+        },{status: 200})
+    } catch (error) {
+        console.log("Error found to delete the messages",error)
+        return Response.json({
+            success: false,
+            message: 'Error deleting message'
+        },{status: 500})
+    }
+}
