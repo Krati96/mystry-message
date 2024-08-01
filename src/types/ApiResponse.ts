64c9b2397203da47f360283767f7f9d5bf769d65
Rishabh <shahbab.alam@bigohtech.com>
2024-08-01 18:07:13 +0530

    Build User DashBoard

--- src/types/ApiResponse.ts
+++ src/types/ApiResponse.ts
@@ -3,6 +3,6 @@ import { Message } from "@/model/User";
 export interface ApiResponse{
     success : boolean;
     message : string;
-    isAcceptingMessages? : boolean; //here ? indicates its optional field
+    isAcceptingMessage? : boolean; //here ? indicates its optional field
     messages? : Array<Message>;
 }
